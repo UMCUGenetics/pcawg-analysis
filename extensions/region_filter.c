b01a5e1b310c79e361053d031afacb2ae7fbd6fd
Roel Janssen <roel@gnu.org>
2020-10-28 18:03:37 +0100

    Add 'extract-region' subcommand.
    * Makefile.am: Register new files to the build system.
    * acontrol.in: Add bits for the ‘extract-region’ subcommand.
    * configure.ac: Register new files; bump version number.
    * extensions/Makefile.am: Add bits for ‘region_filter.c’.
    * extensions/region_filter.c: New file.
    * pcawg/bam-regions.scm.in: New file.
    * ui/extract-region.scm: New file.

---
+++ extensions/region_filter.c
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2020  Roel Janssen <roel@gnu.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <htslib/sam.h>
+#include <libguile.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+SCM
+print_bam_file_error (const char *file_name)
+{
+  /* Now this looks quite Lispy, eh? */
+  char error_msg[255];
+  snprintf (error_msg, 255, "Cannot open '%s'.", file_name);
+
+  return (scm_values
+          (scm_list_2
+           (SCM_BOOL_F, scm_from_latin1_string (error_msg))));
+}
+
+SCM
+print_bam_index_file_error (const char *file_name)
+{
+  /* Now this looks quite Lispy, eh? */
+  char error_msg[255];
+  snprintf (error_msg, 255, "Cannot open index for '%s'.", file_name);
+
+  return (scm_values
+          (scm_list_2
+           (SCM_BOOL_F, scm_from_latin1_string (error_msg))));
+}
+
+SCM
+print_bam_header_error (const char *file_name)
+{
+  return (scm_values
+          (scm_list_2
+           (SCM_BOOL_F, scm_from_latin1_string ("Cannot read header."))));
+}
+
+SCM
+extract_reads_for_region (SCM input_scm,
+                          SCM output_scm,
+                          SCM output_format_scm,
+                          SCM region_scm)
+{
+  /* Scheme to C conversions.
+   * -------------------------------------------------------------------- */
+  char *input_file        = scm_to_locale_string (input_scm);
+  char *output_file       = scm_to_locale_string (output_scm);
+  char *output_format     = scm_to_locale_string (output_format_scm);
+  char *region            = scm_to_locale_string (region_scm);
+
+  /* Prepare the buffers needed to read the BAM file.
+   * -------------------------------------------------------------------- */
+  bam_hdr_t *bam_header = NULL;
+  htsFile   *bam_input_stream = NULL;
+  htsFile   *bam_output_stream = NULL;
+  hts_idx_t *bam_index = NULL;
+
+  bam_input_stream = hts_open (input_file, "r");
+  if (!bam_input_stream)
+    return print_bam_file_error (input_file);
+
+  bam_index = sam_index_load (bam_input_stream, input_file);
+  if (bam_index == 0)
+    return print_bam_index_file_error (input_file);
+
+  bam_output_stream = (! strcmp (output_format, "bam"))
+    ? hts_open (output_file, "wb")
+    : hts_open (output_file, "w");
+
+  if (!bam_output_stream)
+    {
+      hts_close (bam_input_stream);
+      return print_bam_file_error (output_file);
+    }
+
+  /* Read/write the SAM/BAM/CRAM header.
+   * -------------------------------------------------------------------- */
+  bam_header = sam_hdr_read (bam_input_stream);
+  if (!bam_header)
+    {
+      hts_close (bam_input_stream);
+      return print_bam_header_error (input_file);
+    }
+
+  int header_write_result = sam_hdr_write (bam_output_stream, bam_header);
+  if (header_write_result < 0)
+    {
+      hts_close (bam_input_stream);
+      hts_close (bam_output_stream);
+      return print_bam_header_error (output_file);
+    }
+
+  /* Read -> filter -> write the SAM/BAM/CRAM reads.
+   * ----------------------------------------------------------------------- */
+
+  hts_itr_t *iterator;
+  iterator = sam_itr_querys (bam_index, bam_header, region);
+  if (iterator == 0)
+    {
+      bam1_t *alignment = bam_init1 ();
+      int state = 0;
+      while ((state = sam_itr_next (bam_input_stream,
+                                    iterator,
+                                    alignment)) >= 0)
+        {
+          if (sam_write1 (bam_output_stream, bam_header, alignment) <= 0)
+            return
+              (scm_values
+               (scm_list_2
+                (SCM_BOOL_F, scm_from_latin1_string
+                 ("An error occurred while processing an alignment record."))));
+        }
+
+      bam_destroy1 (alignment);
+    }
+
+  sam_itr_destroy (iterator);
+
+  free (input_file);
+  free (output_file);
+  free (output_format);
+  free (region);
+
+  bam_hdr_destroy (bam_header);
+  hts_close (bam_input_stream);
+  hts_close (bam_output_stream);
+
+  return (scm_values
+          (scm_list_2
+           (SCM_BOOL_T, SCM_UNDEFINED)));
+}
+
+void
+init_region_filter ()
+{
+  hts_verbose = 0;
+  scm_c_define_gsubr ("extract-reads-for-region", 4, 0, 0, extract_reads_for_region);
+}
---
+++ ui/extract-region.scm
@@ -0,0 +1,124 @@
+(define-module (ui extract-region)
+  #:use-module (ice-9 format)
+  #:use-module (ice-9 getopt-long)
+  #:use-module (ice-9 popen)
+  #:use-module (ice-9 rdelim)
+  #:use-module (ice-9 receive)
+  #:use-module (ice-9 threads)
+  #:use-module (logger)
+  #:use-module (pcawg filesystem)
+  #:use-module (pcawg tools)
+  #:use-module (pcawg bam-regions)
+  #:use-module (srfi srfi-1)
+
+  #:export (do-extract-region))
+
+(define program-options
+  '((report-bucket          (single-char #\b) (value #t))
+    (debug-log              (single-char #\d) (value #t))
+    (error-log              (single-char #\e) (value #t))
+    (simultaneous-donors    (single-char #\t) (value #t))
+    (store-directory        (single-char #\s) (value #t))
+    (region                 (single-char #\r) (value #t))
+    (help                   (single-char #\h) (value #f))))
+
+(define (show-help)
+  (for-each (lambda (line) (display line) (newline))
+   '("Options for 'extract-region':"
+     "  --report-bucket=ARG        -b  The patient report bucket to use."
+     "  --debug-log                -d  Where to write the debug log."
+     "  --error-log                -e  Where to write the error log."
+     "  --simultaneous-donors=ARG  -t  Number of donors to process in parallel."
+     "  --store-directory=ARG      -s  Where to store the data."
+     "  --region=ARG               -r  The region to extract."
+     "  --help                     -h  Show this message."))
+  (exit 0))
+
+(define (donor->extract-region bucket store-directory donor-id region)
+  (let ((done-file (format #f "~a/~a_~a.done" store-directory donor-id region)))
+    (if (file-exists? done-file)
+        #t
+        (let ((input-file  (format #f "~a/~a-from-jar/~aT/aligner/~aT.bam"
+                                   bucket donor-id donor-id donor-id))
+              (output-file (format #f "~a/~aT_~a.bam"
+                                   store-directory donor-id region))
+              (done-file   (format #f "~a/~aT_~a.done"
+                                   store-directory donor-id region)))
+          (log-debug "donor->extract-region" "Reading file: ~s" input-file)
+          (if (file-exists? done-file)
+              #t
+              (receive (success? message)
+                (extract-reads-for-region input-file output-file "bam" region)
+                (if (not success?)
+                    (begin
+                      (log-error "donor->extract-region" "Error: ~s" message)
+                      #f)
+                    (begin
+                      (call-with-output-file done-file
+                        (lambda (port) (format port "")))
+                      (log-debug "donor->extract-region" "Finished: ~s" donor-id)
+                      #t))))))))
+
+(define (donors-from-bucket bucket-uri)
+
+  (define* (donors-from-port port #:optional (output '()))
+    (let ((directory (read-line port))
+          (prefix-length (+ (string-length bucket-uri) 1)))
+      (if (eof-object? directory)
+          (map (lambda (uri) (substring uri prefix-length)) output)
+          (let* ((cut-at    (string-contains directory "-from-jar"))
+                 (remains   (if cut-at  (substring directory 0 cut-at) #f))
+                 (last-char (if remains (string-ref remains (- cut-at 1)) #f))
+                 (donor     (if (or (eq? last-char #\T)
+                                    (eq? last-char #\R))
+                                #f
+                                remains)))
+            (if donor
+                (donors-from-port port (cons donor output))
+                (donors-from-port port output))))))
+
+  (let* ((command (format #f "~a ls ~a" %gsutil bucket-uri))
+         (port    (open-input-pipe command))
+         (output  (donors-from-port port)))
+    (close-port port)
+    output))
+
+(define (do-extract-region options)
+  (let ((config (getopt-long options program-options)))
+
+    (when (assoc-ref config 'help) (show-help))
+
+    (when (assoc-ref config 'debug-log)
+      (set-default-debug-port!
+       (open-file (assoc-ref config 'debug-log) "a")))
+
+    (when (assoc-ref config 'error-log)
+      (set-default-error-port!
+       (open-file (assoc-ref config 'error-log) "a")))
+
+    (unless (assoc-ref config 'simultaneous-donors)
+      (cons `(simultaneous-donors . "1") config))
+
+    (unless (assoc-ref config 'store-directory)
+      (format #t "Please specify the --store-directory.~%")
+      (exit 1))
+
+    (unless (assoc-ref config 'report-bucket)
+      (format #t "Please specify the --report-bucket.~%")
+      (exit 1))
+
+    (unless (assoc-ref config 'region)
+      (format #t "Please specify the --region.~%")
+      (exit 1))
+
+    (log-debug "extract-region" "Started.")
+    (log-error "extract-region" "Started.")
+    (let ((donors (donors-from-bucket (assoc-ref config 'report-bucket))))
+      (n-par-for-each (string->number (assoc-ref config 'simultaneous-donors))
+                      (lambda (id)
+                        (donor->extract-region
+                         (assoc-ref config 'report-bucket)
+                         (assoc-ref config 'store-directory)
+                         id
+                         (assoc-ref config 'region)))
+                      donors))))

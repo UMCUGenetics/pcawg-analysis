c38dc8ca0bd2eabea7651addc36bc01932551e36
Roel Janssen <roel@gnu.org>
2020-12-02 20:31:27 +0100

    Add ‘verify-unmapped’ subcommand.
    This commit adds the ‘verify-unmapped’ subcommand which aims to
    provide insight into whether the ‘postprocess’ phase has finished
    properly.
    
    This mess is all due to the combination of HTSLib and Google Cloud
    buckets.  Somehow reading from a GCP bucket can end prematurely, and
    no detectable error is given by HTSLib.
    
    * Makefile.am: Register files for the new subcommand.
    * acontrol.in: Register the new subcommand.
    * configure.ac: Register files for the new subcommand.
    * extensions/Makefile.am: Add ‘unmapped_verifier.c’ bits.
    * extensions/unmapped_verifier.c: New file.
    * pcawg/verify-unmapped.scm.in: New file.
    * ui/verify-unmapped.scm: New file.

---
+++ extensions/unmapped_verifier.c
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C) 2020  Roel Janssen <roel@gnu.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <htslib/sam.h>
+#include <libguile.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+SCM
+print_bam_file_error (const char *file_name)
+{
+  /* Now this looks quite Lispy, eh? */
+  char error_msg[255];
+  snprintf (error_msg, 255, "Cannot open '%s'.", file_name);
+
+  return (scm_values
+          (scm_list_3
+           (SCM_BOOL_F,
+            scm_from_latin1_string (error_msg),
+            SCM_UNDEFINED)));
+}
+
+SCM
+print_bam_header_error (const char *file_name)
+{
+  return (scm_values
+          (scm_list_3
+           (SCM_BOOL_F,
+            scm_from_latin1_string ("Cannot read header."),
+            SCM_UNDEFINED)));
+}
+
+SCM
+verify_unmapped_reads (SCM input_scm, SCM output_scm)
+{
+  /* Scheme to C conversions.
+   * -------------------------------------------------------------------- */
+  char *input_file        = scm_to_locale_string (input_scm);
+  char *output_file       = scm_to_locale_string (output_scm);
+
+  /* Prepare the buffers needed to read the BAM file.
+   * -------------------------------------------------------------------- */
+  bam_hdr_t *bam_input_header = NULL;
+  bam_hdr_t *bam_output_header = NULL;
+  htsFile   *bam_input_stream = NULL;
+  htsFile   *bam_output_stream = NULL;
+
+  bam_input_stream = hts_open (input_file, "r");
+  if (!bam_input_stream)
+    return print_bam_file_error (input_file);
+
+  bam_output_stream = hts_open (output_file, "r");
+  if (!bam_output_stream)
+    {
+      hts_close (bam_input_stream);
+      return print_bam_file_error (output_file);
+    }
+
+  /* Read/write the SAM/BAM/CRAM header.
+   * -------------------------------------------------------------------- */
+  bam_input_header = sam_hdr_read (bam_input_stream);
+  if (!bam_input_header)
+    {
+      hts_close (bam_input_stream);
+      return print_bam_header_error (input_file);
+    }
+
+  bam_output_header = sam_hdr_read (bam_output_stream);
+  if (!bam_output_header)
+    {
+      hts_close (bam_output_stream);
+      return print_bam_header_error (output_file);
+    }
+
+  /* Use the index to obtain the total number of unmapped reads. */
+  hts_idx_t *bam_input_index       = NULL;
+  uint64_t input_unmapped_reads    = 0;
+  uint64_t observed_unmapped_reads = 0;
+
+  bam_input_index = sam_index_load (bam_input_stream, input_file);
+  input_unmapped_reads = hts_idx_get_n_no_coor (bam_input_index);
+  hts_idx_destroy (bam_input_index);
+
+  bam1_t *alignment = bam_init1();
+  while (sam_read1 (bam_output_stream, bam_output_header, alignment) > 0)
+    {
+      if ((alignment->core.flag & BAM_FUNMAP))
+        observed_unmapped_reads++;
+    }
+
+  free (input_file);
+  free (output_file);
+
+  bam_destroy1 (alignment);
+  bam_hdr_destroy (bam_input_header);
+  bam_hdr_destroy (bam_output_header);
+  hts_close (bam_input_stream);
+  hts_close (bam_output_stream);
+
+  return (scm_values
+          (scm_list_3
+           (SCM_BOOL_T,
+            scm_from_uint64 (input_unmapped_reads),
+            scm_from_uint64 (observed_unmapped_reads))));
+}
+
+void
+init_unmapped_verifier ()
+{
+  hts_verbose = 0;
+  scm_c_define_gsubr ("verify-unmapped-reads", 2, 0, 0, verify_unmapped_reads);
+}
---
+++ ui/verify-unmapped.scm
@@ -0,0 +1,150 @@
+(define-module (ui verify-unmapped)
+  #:use-module (ice-9 format)
+  #:use-module (ice-9 getopt-long)
+  #:use-module (ice-9 popen)
+  #:use-module (ice-9 rdelim)
+  #:use-module (ice-9 receive)
+  #:use-module (ice-9 threads)
+  #:use-module (logger)
+  #:use-module (pcawg filesystem)
+  #:use-module (pcawg tools)
+  #:use-module (pcawg google)
+  #:use-module (pcawg verify-unmapped)
+  #:use-module (srfi srfi-1)
+
+  #:export (do-verify-unmapped))
+
+(define program-options
+  '((report-bucket          (single-char #\b) (value #t))
+    (debug-log              (single-char #\d) (value #t))
+    (error-log              (single-char #\e) (value #t))
+    (simultaneous-donors    (single-char #\t) (value #t))
+    (store-directory        (single-char #\s) (value #t))
+    (help                   (single-char #\h) (value #f))))
+
+(define (show-help)
+  (for-each (lambda (line) (display line) (newline))
+   '("Options for 'verify-unmapped':"
+     "  --report-bucket=ARG        -b  The patient report bucket to use."
+     "  --debug-log                -d  Where to write the debug log."
+     "  --error-log                -e  Where to write the error log."
+     "  --simultaneous-donors=ARG  -t  Number of donors to process in parallel."
+     "  --store-directory=ARG      -s  Where to store the data."
+     "  --help                     -h  Show this message."))
+  (exit 0))
+
+(define (donor->verify-unmapped-reads bucket storedir donor-id)
+
+  (define (extract-unmapped suffix)
+    (let* ((full-id     (string-append donor-id suffix))
+           (input-file  (format #f "~a/~a-from-jar/~a/aligner/~a.bam"
+                                bucket donor-id full-id full-id))
+           (output-file (format #f "~a/~a_unmapped.bam" storedir full-id))
+           (done-file   (format #f "~a/~a_unmapped.done" storedir full-id)))
+      (if (not (file-exists? done-file))
+          (log-debug "donor->verify-unmapped-reads"
+                     "~s hasn't been post-processed yet." full-id)
+          (receive (success? total-unmapped observed-unmapped)
+              (verify-unmapped-reads input-file output-file)
+            (if (not success?)
+                (begin
+                  (log-error "donor->verify-unmapped-reads"
+                             "Error: ~s" total-unmapped)
+                  #f)
+                (begin
+                  (log-debug "donor->unmapped-reads"
+                             "~s has ~a reads without a coordinate, and ~a were found."
+                             full-id total-unmapped observed-unmapped)
+                  #t))))))
+
+  (extract-unmapped "T")
+  (extract-unmapped "R"))
+
+(define (donors-from-bucket bucket-uri)
+
+  (define* (donors-from-port port #:optional (output '()))
+    (let ((directory (read-line port))
+          (prefix-length (+ (string-length bucket-uri) 1)))
+      (if (eof-object? directory)
+          (map (lambda (uri) (substring uri prefix-length)) output)
+          (let* ((cut-at    (string-contains directory "-from-jar"))
+                 (remains   (if cut-at  (substring directory 0 cut-at) #f))
+                 (last-char (if remains (string-ref remains (- cut-at 1)) #f))
+                 (donor     (if (or (eq? last-char #\T)
+                                    (eq? last-char #\R))
+                                #f
+                                remains)))
+            (if donor
+                (donors-from-port port (cons donor output))
+                (donors-from-port port output))))))
+
+  (let* ((command (format #f "~a ls ~a" %gsutil bucket-uri))
+         (port    (open-input-pipe command))
+         (output  (donors-from-port port)))
+    (close-port port)
+    output))
+
+(define* (process-jobs donors number-of-jobs report-bucket store-directory
+                       #:optional (threads '()))
+  (cond
+   ((or (>= (length threads) number-of-jobs)
+        (null? donors))
+
+    ;; Wait for the current jobs to finish.
+    (while (any not (map thread-exited? threads))
+      (sleep 1))
+
+    ;; Process the remaining donors.
+    (if (null? donors)
+        #t
+        (begin
+          ;; Refresh the auth token.
+          ;; It sets the environment variable GCS_OAUTH_TOKEN as side effect,
+          ;; so we don't need to do anything else.
+          (log-debug "process-jobs" "Token expires in ~a seconds."
+                     (token-expires-in (get-token)))
+
+          ;; Continue processing more donors.
+          (process-jobs donors number-of-jobs report-bucket store-directory '()))))
+
+   ;; Spawn multiple jobs.
+   (else
+    (let ((donor (car donors)))
+      (process-jobs (cdr donors) number-of-jobs report-bucket store-directory
+                    (cons (call-with-new-thread
+                           (lambda _
+                             (donor->verify-unmapped-reads
+                              report-bucket store-directory donor)))
+                          threads))))))
+
+(define (do-verify-unmapped options)
+  (let ((config (getopt-long options program-options)))
+
+    (when (assoc-ref config 'help) (show-help))
+
+    (when (assoc-ref config 'debug-log)
+      (set-default-debug-port!
+       (open-file (assoc-ref config 'debug-log) "a")))
+
+    (when (assoc-ref config 'error-log)
+      (set-default-error-port!
+       (open-file (assoc-ref config 'error-log) "a")))
+
+    (unless (assoc-ref config 'simultaneous-donors)
+      (cons `(simultaneous-donors . "1") config))
+
+    (unless (assoc-ref config 'store-directory)
+      (format #t "Please specify the --store-directory.~%")
+      (exit 1))
+
+    (unless (assoc-ref config 'report-bucket)
+      (format #t "Please specify the --report-bucket.~%")
+      (exit 1))
+
+    (log-debug "verify-unmapped" "Started.")
+    (log-error "verify-unmapped" "Started.")
+    (let ((donors (donors-from-bucket (assoc-ref config 'report-bucket))))
+      (process-jobs donors
+                    (string->number (assoc-ref config 'simultaneous-donors))
+                    (assoc-ref config 'report-bucket)
+                    (assoc-ref config 'store-directory)))))

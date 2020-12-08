b429f61ec34d8c86b636e9b1511c08e3470f891b
Roel Janssen <roel@gnu.org>
2020-12-08 17:37:34 +0100

    extensions: region_filter: Simplify.
    This commit golfs a bit through the code and simplifies the allocation
    and deallocation of various items.
    
    * extensions/region_filter.c: Simplify allocation and deallocation throughout.

--- extensions/region_filter.c
+++ extensions/region_filter.c
@@ -30,7 +30,7 @@ print_bam_file_error (char *file_name)
   char error_msg[255];
   snprintf (error_msg, 255, "Cannot open '%s'.", file_name);
 
-  free (file_name);
+  free (file_name); file_name = NULL;
 
   return (scm_values
           (scm_list_2
@@ -44,7 +44,7 @@ print_bam_index_file_error (char *file_name)
   char error_msg[255];
   snprintf (error_msg, 255, "Cannot open index for '%s'.", file_name);
 
-  free (file_name);
+  free (file_name); file_name = NULL;
 
   return (scm_values
           (scm_list_2
@@ -65,50 +65,44 @@ extract_reads_for_region (SCM input_scm,
                           SCM output_format_scm,
                           SCM region_scm)
 {
-  /* Scheme to C conversions.
-   * -------------------------------------------------------------------- */
-  char *input_file        = scm_to_locale_string (input_scm);
-  char *output_file       = scm_to_locale_string (output_scm);
-  char *output_format     = scm_to_locale_string (output_format_scm);
-  char *region            = scm_to_locale_string (region_scm);
-
   /* Prepare the buffers needed to read the BAM file.
    * -------------------------------------------------------------------- */
-  bam_hdr_t *bam_header = NULL;
-  htsFile   *bam_input_stream = NULL;
-  htsFile   *bam_output_stream = NULL;
-  hts_idx_t *bam_index = NULL;
 
+  bam_hdr_t *bam_header        = NULL;
+  htsFile   *bam_input_stream  = NULL;
+  htsFile   *bam_output_stream = NULL;
+  hts_idx_t *bam_index         = NULL;
+  hts_itr_t *iterator          = NULL;
+  char      *input_file        = NULL;
+  char      *output_file       = NULL;
+  char      *output_format     = NULL;
+  char      *region            = NULL;
+  SCM       output             = NULL;
+  int       state              = 0;
+
+  input_file = scm_to_locale_string (input_scm);
   bam_input_stream = hts_open (input_file, "r");
   if (! bam_input_stream)
-    {
-      free (output_file);
-      free (output_format);
-      free (region);
-
-      return print_bam_file_error (input_file);
-    }
+    return print_bam_file_error (input_file);
 
   bam_index = sam_index_load (bam_input_stream, input_file);
   if (! bam_index)
-    {
-      free (output_file);
-      free (output_format);
-      free (region);
+    return print_bam_index_file_error (input_file);
 
-      return print_bam_index_file_error (input_file);
-    }
+  output_format = scm_to_locale_string (output_format_scm);
+  output_file   = scm_to_locale_string (output_scm);
 
   bam_output_stream = (! strcmp (output_format, "bam"))
     ? hts_open (output_file, "wb")
     : hts_open (output_file, "w");
 
-  free (output_format);
+  free (output_format); output_format = NULL;
+
   if (! bam_output_stream)
     {
-      free (input_file);
-      free (region);
-      hts_close (bam_input_stream);
+      free (input_file);             input_file = NULL;
+      hts_close (bam_input_stream);  bam_input_stream = NULL;
+      hts_idx_destroy (bam_index);   bam_index = NULL;
 
       return print_bam_file_error (output_file);
     }
@@ -116,27 +110,14 @@ extract_reads_for_region (SCM input_scm,
   /* Read/write the SAM/BAM/CRAM header.
    * -------------------------------------------------------------------- */
   bam_header = sam_hdr_read (bam_input_stream);
-  if (! bam_header)
+  if ((! bam_header) || (sam_hdr_write (bam_output_stream, bam_header) < 0))
     {
-      free (input_file);
-      free (output_file);
-      free (region);
+      free (input_file);             input_file = NULL;
+      hts_close (bam_input_stream);  bam_input_stream = NULL;
+      hts_idx_destroy (bam_index);   bam_index = NULL;
 
-      hts_close (bam_input_stream);
-      hts_close (bam_output_stream);
-
-      return print_bam_header_error ();
-    }
-
-  int header_write_result = sam_hdr_write (bam_output_stream, bam_header);
-  if (header_write_result < 0)
-    {
-      free (input_file);
-      free (output_file);
-      free (region);
-
-      hts_close (bam_input_stream);
-      hts_close (bam_output_stream);
+      hts_close (bam_input_stream);  bam_input_stream = NULL;
+      hts_close (bam_output_stream); bam_output_stream = NULL;
 
       return print_bam_header_error ();
     }
@@ -144,10 +125,9 @@ extract_reads_for_region (SCM input_scm,
   /* Read -> filter -> write the SAM/BAM/CRAM reads.
    * ----------------------------------------------------------------------- */
 
-  SCM output = NULL;
+  region = scm_to_locale_string (region_scm);
 
-  hts_itr_t *iterator = sam_itr_querys (bam_index, bam_header, region);
-  int state = 0;
+  iterator = sam_itr_querys (bam_index, bam_header, region);
   if (iterator != 0)
     {
       bam1_t *alignment = bam_init1 ();
@@ -164,9 +144,9 @@ extract_reads_for_region (SCM input_scm,
 
               break;
             }
-        }
+    	}
 
-      bam_destroy1 (alignment);
+      bam_destroy1 (alignment); alignment = NULL;
     }
   else
     output = scm_values
@@ -174,16 +154,16 @@ extract_reads_for_region (SCM input_scm,
                (SCM_BOOL_F, scm_from_latin1_string
                 ("Cannot find region.")));
 
-  sam_itr_destroy (iterator);
-  hts_idx_destroy (bam_index); bam_index = NULL;
+  sam_itr_destroy (iterator);    iterator = NULL;
+  hts_idx_destroy (bam_index);   bam_index = NULL;
 
-  free (input_file);
-  free (output_file);
-  free (region);
+  free (input_file);             input_file = NULL;
+  free (output_file);            output_file = NULL;
+  free (region);                 region = NULL;
 
-  bam_hdr_destroy (bam_header);
-  hts_close (bam_input_stream);
-  hts_close (bam_output_stream);
+  bam_hdr_destroy (bam_header);  bam_header = NULL;
+  hts_close (bam_input_stream);  bam_input_stream = NULL;
+  hts_close (bam_output_stream); bam_output_stream = NULL;
 
   if (output == NULL)
     {
